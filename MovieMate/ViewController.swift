4d61f6eaa4026f5b0045d545b83b84b0773eafdc
rcwoshimao <mantoumiaoshen@gmail.com>
2023-02-28 23:31:20 -0800

    Initial Commit

---
+++ MovieMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMate
+//
+//  Created by Rebecca Chen on 2023/2/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
